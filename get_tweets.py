b6beb90b23dd13a42d54311444713669712b54be
james <jameslathamsmith@gmail.com>
2019-05-01 15:29:24 -0700

    Added import from auth file to stop authentication from being on Github, and added .gitignore

--- get_tweets.py
+++ get_tweets.py
@@ -1,18 +1,18 @@
 import tweepy
+
+from auth import consumer_key, consumer_secret, access_token, access_token_secret
+
+
 def create_api():
-    consumer_key = "XXXX"
-    consumer_secret = "XXXX"
-    access_token = "XXXX"
-    access_token_secret = "XXXX"
-    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-    auth.set_access_token(access_token,access_token_secret)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     return api
+
+
 api = create_api()
-results = api.search(q="metoo",lang="en")
+results = api.search(q="metoo", lang="en")
 i = 1
 for tweet in results:
-    print (i,")))",tweet.user.screen_name,"Tweeted : ", tweet.text)
+    print(i, ")))", tweet.user.screen_name, "Tweeted : ", tweet.text)
     i = i+1
-
-
