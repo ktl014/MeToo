8c6baab0dd2731eebf0e9f940dc7d842788beb58
james <jameslathamsmith@gmail.com>
2019-06-07 19:58:09 -0700

    Updated utilities and visualizion notebook

--- utilities.py
+++ utilities.py
@@ -1,7 +1,6 @@
 # Import authentication stuffs
 from auth import consumer_key, consumer_secret, access_token, access_token_secret
 import tweepy
-import tweepy
 import pickle
 import numpy as np
 import datetime
@@ -18,26 +17,29 @@ from shapely.geometry import Point
 
 import numpy as np
 
-from bokeh.io import curdoc
-from bokeh.layouts import column
-from bokeh.models import ColumnDataSource, Slider
-from bokeh.plotting import figure
-
 
 import imageio
 
-# From https://stackoverflow.com/questions/20169467/how-to-convert-from-longitude-and-latitude-to-country-or-city
-data = requests.get(
-    "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson").json()
-
-countries = {}
-for feature in data["features"]:
-    geom = feature["geometry"]
-    country = feature["properties"]["ADMIN"]
-    countries[country] = prep(shape(geom))
+from geopy.geocoders import Nominatim
+import pandas as pd
 
 
-def get_country(lon, lat):
+def create_country_dict():
+    ''' Generates a dictionary that maps countries to polygons '''
+    data = requests.get(
+        "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson").json()
+
+    countries = {}
+    for feature in data["features"]:
+        geom = feature["geometry"]
+        country = feature["properties"]["ADMIN"]
+        countries[country] = prep(shape(geom))
+    return countries
+
+
+def get_country(lon, lat, countries):
+    ''' Given a longitude latitude of a tweet, and the country dictionary,
+    extracts the country name '''
     point = Point(lon, lat)
     for country, geom in countries.items():
         if geom.contains(point):
@@ -47,6 +49,8 @@ def get_country(lon, lat):
 
 
 def get_retweet_by_user_date(date_retweets_dict, user, date):
+    ''' Extracts a retweet given the user, date and a dictionary containing
+    dates to retweets '''
     retweet_cnt_user = date_retweets_dict[date]
 
     for retweet_user in retweet_cnt_user:
@@ -120,7 +124,8 @@ def create_api():
     return api
 
 
-def plot_locations(lat_longs, plot_us=True, date_retweets_dict=None, over_time=True, save_every=None, batch_size=100, color_gradient=False):
+def plot_locations(lat_longs, plot_us=True, date_retweets_dict=None, save_every=None, batch_size=100, color_gradient=False):
+    ''' Plots a map of either US or International to visualize #metoo tweets. Scales each tweet by its retweet count'''
     if(plot_us):
         filename = "images/us/"
         scalar = .5
@@ -150,6 +155,7 @@ def plot_locations(lat_longs, plot_us=True, date_retweets_dict=None, over_time=T
         m.drawcountries(linewidth=0.5)
 
     count = 1
+    # Go through each batch and plot
     while(count + batch_size <= len(lat_longs)):
         current_batch = lat_longs[count:count+batch_size]
 
@@ -179,23 +185,27 @@ def plot_locations(lat_longs, plot_us=True, date_retweets_dict=None, over_time=T
             # Only size changes
             m.scatter(lats, lons, latlon=True, marker='o',
                       c='r', s=retweet_cnts, cmap=plt.get_cmap('seismic'), alpha=.1)
-        plt.pause(0.0001)
+        # Uncomment for live viewing
+        # plt.pause(0.0001)
 
         if(save_every is not None):
             if(count % save_every == 0):
                 plt.savefig(filename + str(count).zfill(5) + ".png")
-        print(date)
+
+        # print(date)
         count += batch_size
-    # plt.show()
+    plt.show()
 
 
-def filter_lat_longs(lat_longs, us_only=False):
+def filter_lat_longs(lat_longs, countries, us_only):
+    ''' Filters latitude and longitudes to remove Nones and if only looking for US,
+    only reture lat longs that are within US '''
     filtered_lat_longs = []
 
     for lat_long in lat_longs:
         if lat_long[1][0] is not None:
             # If we only want US lat, longs, then we need to check which country they are in
-            if(us_only and ("America" in get_country(lat_long[1][1], lat_long[1][0]))):
+            if(us_only and ("America" in get_country(lat_long[1][1], lat_long[1][0], countries))):
                 filtered_lat_longs.append(lat_long)
             elif (not us_only):
                 filtered_lat_longs.append(lat_long)
@@ -204,10 +214,10 @@ def filter_lat_longs(lat_longs, us_only=False):
 
 
 def generate_gif(directory):
+    ''' Generates a gif from the files in directory '''
     images = []
     filenames = os.listdir(directory)
     filenames = sorted(filenames)
-    print(filenames)
     for filename in filenames:
         if(".png" in filename):
             images.append(imageio.imread(directory + filename))
@@ -216,5 +226,200 @@ def generate_gif(directory):
                     format='GIF', duration=.1)
 
 
-def display_gif_bokeh():
-    pass
+def extract_retweet_numbers(df):
+    ''' Extracts retweet numbers from dataframe '''
+    retweet_cnts = df['retweets']
+    dates = df['date']
+    users = df['user']
+
+    date_to_retweets_dict = {}
+
+    for i in range(len(retweet_cnts)):
+        if(dates[i]) not in date_to_retweets_dict:
+            date_to_retweets_dict[dates[i]] = [(users[i], retweet_cnts[i])]
+        else:
+            date_to_retweets_dict[dates[i]].append((users[i], retweet_cnts[i]))
+
+    return date_to_retweets_dict
+
+
+def extract_locations():
+    ''' Extracts user names from tweet csv file and then uses usernames
+    to get their locations via tweepy'''
+    # Create the tweepy api to extract username info (location)
+    api = create_api()
+
+    # Create the geolocator to extract latitude and longitude from user location
+    geolocator = Nominatim(user_agent="metwoo_data_analysis")
+
+    # Read in dataframe
+    df = pd.read_csv('datasets/tweet_2017_to_2018_with_hashtags.csv')
+
+    # Extract locations from each tweet
+    extract_locations_from_file(api, geolocator, df, num_locs=100)
+
+
+def get_dates_and_retweets():
+    ''' Gets dates and their associated retweets from csv files to combine them into
+    a single dictionary '''
+    # Uncomment to extract locations from file (Otherwise, they will are in the repo)
+    # extract_locations()
+    df = pd.read_csv('datasets/tweet_2017_to_2018_with_hashtags.csv')
+    date_retweets_2017 = extract_retweet_numbers(df)
+
+    df = pd.read_csv('datasets/tweet_2018_to_2019_with_hashtags.csv')
+    date_retweets_2018 = extract_retweet_numbers(df)
+
+    date_retweets_dict = {**date_retweets_2017, **date_retweets_2018}
+
+    return date_retweets_dict
+
+
+def format_lat_longs(countries):
+    ''' Reverses and combines lat longs, and extracts us only lat_longs
+    to return '''
+    lat_longs_2017 = pickle.load(open("lat_long_2017.p", "rb"))
+    lat_longs_2018 = pickle.load(open("lat_long_2018_2019.p", "rb"))
+
+    lat_longs_2017.reverse()
+    lat_longs_2018.reverse()
+
+    lat_longs_orig = lat_longs_2017 + lat_longs_2018
+
+    lat_longs = filter_lat_longs(lat_longs_orig, countries, us_only=False)
+
+    us_lat_longs = filter_lat_longs(lat_longs_orig, countries, us_only=True)
+
+    return lat_longs, us_lat_longs
+
+
+def get_retweets_by_country(lat_longs, date_retweets_dict, countries):
+    ''' Extracts the number of retweets associated with each country '''
+    tweet_by_country_dict = {}
+    retweets_by_country_dict = {}
+
+    # Detetmine the country of each lat long
+    for lat_long in lat_longs:
+        country = get_country(
+            lon=lat_long[1][1], lat=lat_long[1][0], countries=countries)
+        if country in tweet_by_country_dict:
+            tweet_by_country_dict[country].append(lat_long)
+        else:
+            tweet_by_country_dict[country] = [lat_long]
+
+    # For each country, determine the sum of retweets
+    for country in tweet_by_country_dict:
+        for tweet in tweet_by_country_dict[country]:
+            retweets = get_retweet_by_user_date(
+                date_retweets_dict, tweet[0], tweet[2])
+            if(country in retweets_by_country_dict):
+                retweets_by_country_dict[country] += retweets
+            else:
+                retweets_by_country_dict[country] = retweets
+
+    return retweets_by_country_dict
+
+
+def get_minimal_retweets_by_country(retweets_by_country_dict):
+    ''' Gets a subset of retweets by country based on the retweet_cnt '''
+    minimal_retweets_by_country_dict = {}
+
+    minimal_retweets_by_country_dict["Other"] = retweets_by_country_dict.pop(
+        "unknown")
+
+    minimal_retweets_by_country_dict["United States"] = retweets_by_country_dict.pop(
+        "United States of America")
+
+    for country in retweets_by_country_dict:
+        retweet_cnt = retweets_by_country_dict[country]
+        if(retweet_cnt < 5000):
+            minimal_retweets_by_country_dict["Other"] += retweet_cnt
+        else:
+            minimal_retweets_by_country_dict[country] = retweet_cnt
+
+    sorted_minimal_retweets_by_country = sorted(minimal_retweets_by_country_dict.items(),
+                                                key=lambda kv: kv[1], reverse=True)
+    return sorted_minimal_retweets_by_country
+
+
+def generate_retweets_plots(sorted_minimal_retweets_by_country):
+    ''' Generates pie charts and bar graphs for retweets by country visualization '''
+    labels = []
+    slices = []
+
+    for country_retweet_tup in sorted_minimal_retweets_by_country:
+        labels.append(country_retweet_tup[0])
+        slices.append(country_retweet_tup[1])
+
+    # Plot the retweets by country bar chart
+    fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(aspect="equal"))
+
+    wedges, texts, autotexts = plt.pie(slices[:5], autopct='%1.1f%%')
+    plt.setp(autotexts, size=16, weight="bold")
+    ax.legend(wedges, labels,
+              title="Country",
+              loc="center left",
+              bbox_to_anchor=(1, 0, 0.5, 1),
+              prop={'size': 16})
+    ax.set_title("Number of #metoo Retweets by Country")
+    plt.show()
+
+    # For bar charts, we don't want the "unknown" label so discard it
+    bar_labels = labels[0:2] + labels[3:8]
+    bar_slices = slices[0:2] + slices[3:8]
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    # Normalize the retweets
+    bar_slices = [bar_slice / bar_slices[0]
+                  for bar_slice in bar_slices]
+
+    # Change United States to US and United Kingdom to UK
+    bar_labels[0] = "US"
+    bar_labels[2] = "UK"
+
+    # Plot retweets by country bar chart
+    ax.barh(bar_labels, bar_slices, align='center')
+    ax.set_yticklabels(bar_labels, fontsize=16)
+    plt.show()
+
+    # From wikipedia
+    english_speakers = [
+        283160411,  # America
+        125344737,  # India
+        59600000,  # UK
+        29973590,  # Canada
+        5900000,  # Romania
+        17357833,  # Australia
+        28416,  # Kyrgyzstan
+        50000000  # Pakistan
+    ]
+
+    # Normalize english_speakers to US
+    english_speakers = [english_speaker / english_speakers[0]
+                        for english_speaker in english_speakers]
+
+    # Plot english speakers by country bar chart
+    fig, ax = plt.subplots(figsize=(6, 4))
+    ax.barh(bar_labels, english_speakers[:7], align='center')
+    ax.set_yticklabels(bar_labels, fontsize=16)
+    plt.show()
+
+
+# Test Code
+"""
+
+
+countries = create_country_dict()
+
+lat_longs, us_lat_longs = format_lat_longs(countries)
+date_retweets_dict = get_dates_and_retweets()
+
+retweets_by_country_dict = get_retweets_by_country(
+    lat_longs, date_retweets_dict, countries)
+
+plot_locations(lat_longs, plot_us=False, date_retweets_dict=date_retweets_dict,
+               save_every=100, batch_size=100, color_gradient=False)
+
+
+generate_gif("images/global/")
+"""
